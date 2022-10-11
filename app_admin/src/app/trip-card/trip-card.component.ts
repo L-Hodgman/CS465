1c07d445ec278021a2623ac64f460a3d3fec882d
Elizabeth Hodgman <Elizabeth.Hodgman@snhu.edu>
2022-10-11 17:02:55 -0500

    Added Authorization Components to the API for Admins

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication';
 
 @Component({
   selector: 'app-trip-card',
@@ -11,11 +12,17 @@ export class TripCardComponent implements OnInit {
 
   @Input('trip') trip: any;
   constructor(
+    private authService: AuthenticationService,
     private router: Router
   ) { }
 
   ngOnInit() {
   }
+
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
   private editTrip(trip: Trip): void {
     console.log('Inside TripListingComponent#editTrip');
     localStorage.removeItem("tripCode");
